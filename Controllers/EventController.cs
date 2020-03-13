f36721704bd6c2ca977b9d00d71961184a41bf86
wesin ribeiro <wesinec@gmail.com>
2020-03-13 16:46:45 -0300

    change event controller

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using iatec.Data;
 using iatec.Models;
+using System.Linq;
 
 namespace iatec.Controllers
 {
@@ -13,11 +14,34 @@ namespace iatec.Controllers
     {
         [HttpGet]
         [Route("")]
-        public async Task<ActionResult<List<Event>>> Get([FromServices] DataContext context)
+        public async Task<ActionResult<List<Event>>> GetEvents([FromServices] DataContext context)
         {
             var events = await context.Events.ToListAsync();
             return events;
         }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Event>> GetEventsById([FromServices] DataContext context, int id)
+        {
+            var ev = await context.Events.Include(x => x.User)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            return ev;
+        }
+
+        [HttpGet]
+        [Route("users/{id:int}")]
+        public async Task<ActionResult<List<Event>>> GetEventsByUser([FromServices] DataContext context, int id)
+        {
+            var events = await context.Events.Include(x => User)
+                .Include(x => x.Id == id)
+                .AsNoTracking()
+                .Where(x => x.UserId == id)
+                .ToListAsync();
+            return events;
+        }
+
 
         [HttpPost]
         [Route("")]
