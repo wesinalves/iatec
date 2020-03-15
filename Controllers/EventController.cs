e1ac913973dd72ecff540793d803bbe3c34564a0
wesin ribeiro <wesinec@gmail.com>
2020-03-15 11:17:50 -0300

    add data seed

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -61,5 +61,6 @@ namespace iatec.Controllers
             }
 
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using iatec.Data;
 
 namespace iatec
 {
@@ -16,5 +20,28 @@ namespace iatec
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        
+        private static void SeedDatabase(IHost host)
+        {
+            var scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
+
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+                if (context.Database.EnsureCreated())
+                {
+                    try
+                    {
+                        SeedData.Initialize(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                        logger.LogError(ex, "A database seeding error occurred.");
+                    }
+                }
+            }
+        }
     }
 }
