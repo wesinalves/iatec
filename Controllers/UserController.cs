c6614e827b5fb2cdf3a093182487d4c9dca18247
wesin ribeiro <wesinec@gmail.com>
2020-03-15 11:50:18 -0300

    udpade user controller with scafold

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using iatec.Data;
@@ -7,35 +10,101 @@ using iatec.Models;
 
 namespace iatec.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("v1/users")]
-    public class UserController: ControllerBase
+    public class UserController : ControllerBase
     {
-        [HttpGet]
-        [Route("")]
-        public async Task<ActionResult<List<User>>> Get([FromServices] DataContext context)
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
         {
-            var users = await context.Users.ToListAsync();
-            return users;
+            _context = context;
         }
 
-        [HttpPost]
-        [Route("")]
-        public async Task<ActionResult<User>> Post(
-            [FromServices] DataContext context,
-            [FromBody]User model)
+        // GET: api/User
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            if (ModelState.IsValid)
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
             {
-                context.Users.Add(model);
-                await context.SaveChangesAsync();
-                return model;
-            }
-            else
-            {
-                return BadRequest(ModelState);
+                return NotFound();
             }
 
+            return user;
+        }
+
+        // PUT: api/User/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/User
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.Id == id);
         }
     }
 }
